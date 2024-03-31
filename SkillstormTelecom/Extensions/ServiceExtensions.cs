69ee2c87fc6009d0835a12f1f6eb6ca84cd549a0
Bram <b93ram@gmail.com>
2024-03-31 14:47:03 -0400

    Added Initial project structure with Extension for Cors

---
+++ SkillstormTelecom/Extensions/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace SkillstormTelecom.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)=>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+                });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>  
+            services.Configure<IISOptions>(options =>
+            {
+              //  options.AutomaticAuthentication = false;
+              //  options.AuthenticationDisplayName = "Windows";
+              //  options.ForwardClientCertificate = false;
+              //  options.ForwardWindowsAuthentication = false;
+            });
+    }
+}
