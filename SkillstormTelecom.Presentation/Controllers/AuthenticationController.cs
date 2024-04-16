d99cd40d09e4e6c68fa3db5526034c981b23360c
Bram <b93ram@gmail.com>
2024-04-15 23:52:36 -0400

    Trying to integrate authenticated user with pre-existing logic

---
+++ SkillstormTelecom.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,50 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+
+namespace ContosoUniversity.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public AuthenticationController(IServiceManager serviceManager) => _service = serviceManager;
+
+        [HttpPost]
+
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            var result = await _service.Authentication.RegisterUser(userForRegistration);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+            return Created();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Authenticate([FromBody] UserForAuthenticationDto userForAuthentication)
+        {
+            if (!await _service.Authentication.ValidateUser(userForAuthentication))
+            {
+                return Unauthorized();
+            }
+
+            var tokenDto = await _service.Authentication.CreateToken(populateExp: true);
+
+            return Ok(tokenDto);
+        }
+    }
+}*/
---
+++ SkillstormTelecom.Presentation/Controllers/TokenController.cs
@@ -0,0 +1,28 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+
+namespace ContosoUniversity.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public TokenController(IServiceManager service) => _service = service;
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh([FromBody] TokenDto tokenDto)
+        {
+            var tokenDtoToReturn = await _service.Authentication.RefreshToken(tokenDto);
+            return Ok(tokenDtoToReturn);
+        }
+    }
+}
+*/
